fd64b073a388c9cf55678064cd5514fa49d40c84
MichaelVessia <michaelvessia@mail.adelphi.edu>
2015-08-08 02:34:14 -0400

    added username, login, following, followers

--- summary.rb
+++ summary.rb
@@ -15,6 +15,10 @@ repos.each { |repo|
 
 
 
+user = github.users.get user: username
+puts "#{user['name']} (#{user['login']})"
+puts "Following: #{user['following']}, Followers: #{user['followers']}"
+puts "\n"
 format="#{star.encode('utf-8')} %-#{max_star_len}s\t%-#{max_name_len}s\t%-#{max_lang_len}s\n"
 repos.each { |repo|
   printf(format, repo['stargazers_count'], repo['name'], repo['language'])
