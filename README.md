49ad66dd693b9f27321cc24b6da91dac33738e2c
MichaelVessia <michaelvessia@mail.adelphi.edu>
2015-08-08 02:02:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#GitHub Summarize
+
+GitHub Summarize is a Ruby script that will give you a brief GitHub summary of any user. Test it out!
+
+##Basic Usage
+
+Call the script with
+
+```
+ruby summary.rb GitHubUsername
+```
---
+++ summary.rb
@@ -0,0 +1,28 @@
+require 'github_api'
+
+username = ARGV[0]
+github = Github.new user: username
+repos = github.repos.list
+
+star = "\u2605"
+
+max_name_len = max_star_len = max_lang_len = 0
+repos.each { |repo| 
+  max_name_len = repo['name'].length if repo['name'].length > max_name_len 
+  max_star_len = repo['stargazers_count'].to_s.length if repo['stargazers_count'].to_s.length > max_star_len
+  max_lang_len = repo['language'].length if repo['language'].length > max_lang_len
+}
+
+
+
+format="#{star.encode('utf-8')} %-#{max_star_len}s\t%-#{max_name_len}s\t%-#{max_lang_len}s\n"
+repos.each { |repo|
+  printf(format, repo['stargazers_count'], repo['name'], repo['language'])
+}
+
+
+
+
+
+
+
