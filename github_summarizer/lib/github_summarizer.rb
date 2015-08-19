eb72607a7f7be931eb37ddab2cc433399f1e2aa0
Michael Vessia <michaelvessia@mail.adelphi.edu>
2015-08-19 16:40:39 -0400

    added gem structure and star color

--- github_summarizer/lib/github_summarizer.rb
+++ github_summarizer/lib/github_summarizer.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'github_api'
+require 'colorize'
 
 username = ARGV[0]
 github = Github.new user: username
@@ -20,10 +21,10 @@ repos.each { |repo|
 
 
 user = github.users.get user: username
-puts "#{user['name']} (#{user['login']})"
+puts "#{user['name'].red} (#{user['login'].red})"
 puts "Following: #{user['following']}, Followers: #{user['followers']}"
 puts "\n"
-format="#{star.encode('utf-8')} %-#{max_star_len}s\t%-#{max_name_len}s\t%-#{max_lang_len}s\n"
+format="#{star.encode('utf-8').yellow} %-#{max_star_len}s\t%-#{max_name_len}s\t%-#{max_lang_len}s\n"
 repos.each { |repo|
   printf(format, repo['stargazers_count'], repo['name'], repo['language'])
 }
---
+++ github_summarizer/lib/github_summarizer/version.rb
@@ -0,0 +1,3 @@
+module GithubSummarizer
+  VERSION = "0.1.0"
+end
