0fce111ca82d145742313d4e65230bf7e99eff31
MichaelVessia <michaelvessia@mail.adelphi.edu>
2015-08-19 18:26:44 -0400

    updates for exe

--- github_summarizer/github_summarizer.gemspec
+++ github_summarizer/github_summarizer.gemspec
@@ -1,7 +1,8 @@
 # coding: utf-8
 lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'github_summarizer/version'
+require File.expand_path('../lib/github_summarizer/version', __FILE__)
+
 
 Gem::Specification.new do |spec|
   spec.name          = "github_summarizer"
@@ -10,20 +11,11 @@ Gem::Specification.new do |spec|
   spec.email         = ["michaelvessia@mail.adelphi.edu"]
 
   spec.summary       = %q{Print a quick summary of a GitHub user.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.homepage      = "https://github.com/MichaelVessia/Summarizer.git"
   spec.license       = "MIT"
 
-  # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
-  # delete this section to allow pushing this gem to any host.
-  if spec.respond_to?(:metadata)
-    spec.metadata['allowed_push_host'] = "TODO: Set to 'http://mygemserver.com'"
-  else
-    raise "RubyGems 2.0 or newer is required to protect against public gem pushes."
-  end
-
-  spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
-  spec.bindir        = "exe"
-  spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
+  spec.files         = `git ls-files`.split($\)
+  spec.executables   = ["github_summarizer"]
   spec.require_paths = ["lib"]
 
   spec.add_development_dependency "bundler", "~> 1.10"
--- github_summarizer/lib/github_summarizer.rb
+++ github_summarizer/lib/github_summarizer.rb
@@ -1,37 +1,35 @@
-#!/usr/bin/env ruby
-
+require  'github_summarizer/version'
 require 'github_api'
 require 'colorize'
 
-username = ARGV[0]
-github = Github.new user: username
-repos = github.repos.list
+module GH 
+  class Summarizer
+    def summarize
+      username = ARGV[0]
+      github = Github.new user: username
+      repos = github.repos.list
 
-star = "\u2605"
+      star = "\u2605"
 
-max_name_len = max_star_len = max_lang_len = 0
-repos.each { |repo| 
-  max_name_len = repo['name'].length if repo['name'].length > max_name_len 
-  max_star_len = repo['stargazers_count'].to_s.length if repo['stargazers_count'].to_s.length > max_star_len
-  if repo['language']
-    max_lang_len = repo['language'].length if repo['language'].length > max_lang_len
+      max_name_len = max_star_len = max_lang_len = 0
+      repos.each { |repo| 
+        max_name_len = repo['name'].length if repo['name'].length > max_name_len 
+        max_star_len = repo['stargazers_count'].to_s.length if repo['stargazers_count'].to_s.length > max_star_len
+        if repo['language']
+          max_lang_len = repo['language'].length if repo['language'].length > max_lang_len
+        end
+      }
+
+
+
+      user = github.users.get user: username
+      puts "#{user['name'].red} (#{user['login'].red})"
+      puts "Following: #{user['following']}, Followers: #{user['followers']}"
+      puts "\n"
+      format="#{star.encode('utf-8').yellow} %-#{max_star_len}s\t%-#{max_name_len}s\t%-#{max_lang_len}s\n"
+      repos.each { |repo|
+        printf(format, repo['stargazers_count'].to_s.yellow, repo['name'], repo['language'].to_s.blue)
+      }
+    end
   end
-}
-
-
-
-user = github.users.get user: username
-puts "#{user['name'].red} (#{user['login'].red})"
-puts "Following: #{user['following']}, Followers: #{user['followers']}"
-puts "\n"
-format="#{star.encode('utf-8').yellow} %-#{max_star_len}s\t%-#{max_name_len}s\t%-#{max_lang_len}s\n"
-repos.each { |repo|
-  printf(format, repo['stargazers_count'], repo['name'], repo['language'])
-}
-
-
-
-
-
-
-
+end
